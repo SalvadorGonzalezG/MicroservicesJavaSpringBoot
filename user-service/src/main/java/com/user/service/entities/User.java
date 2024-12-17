572c7c9f58a8301cc4cbe98e53b230a126eb6bf7
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-12-17 00:17:10 -0600

    Inicializando proyecto con Spring boot microservices

---
+++ user-service/src/main/java/com/user/service/entities/User.java
@@ -0,0 +1,8 @@
+package com.user.service.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity // Mapeamos la clase en una tabla en la base de datos permite a JPA gestionar el ciclo de vida de las entidades como creacion, actualizacion y elimiaciom
+public class User {
+
+}
