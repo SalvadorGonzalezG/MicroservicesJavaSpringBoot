612f751e602d09d9ed86f2132bb752096e4ee91f
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-05-06 23:40:09 -0600

    Inicializando microservicio Player Equipment aplicacion utilizando @PathVariable para mostrar equipos player de forma aleatoria

---
+++ playerEquipment/src/main/java/com/playerEquip/espec/PlayerEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.playerEquip.espec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerEquipmentApplication.class, args);
+	}
+
+}
---
+++ playerEquipment/src/test/java/com/playerEquip/espec/PlayerEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playerEquip.espec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
