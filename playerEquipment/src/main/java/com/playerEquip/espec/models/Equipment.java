f2b4eb843c30ec2b09306e39bc146e009dafa660
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-05-07 00:16:21 -0600

    añadiendo atributos a la clase Equipment un constructor que inicializa una plantilla y un metodo que agrega un player a un arrayList

--- playerEquipment/src/main/java/com/playerEquip/espec/models/Equipment.java
+++ playerEquipment/src/main/java/com/playerEquip/espec/models/Equipment.java
@@ -1,5 +1,36 @@
 package com.playerEquip.espec.models;
 
-public class Equipment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Equipment {
+	//Atributes
+	private String nombre;
+	private List<Player> plantilla;
+	
+	//COnstructor que inicializara la plantilla
+	public Equipment() {
+		plantilla = new ArrayList();
+	}
+	
+	//getters & Setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public List<Player> getPlantilla(){
+		return plantilla;
+	}
+	public void setPlantilla(List<Player> plantilla) {
+		this.plantilla = plantilla;
+	}
+	
+	//metodo para agregar un jugador a la plantilla
+	public void addPlayer(Player player) {
+		plantilla.add(player);
+	}
+	
 }
