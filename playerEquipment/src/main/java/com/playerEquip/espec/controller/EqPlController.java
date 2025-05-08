1dea9850aa27b4b855aa91945016ba11651e8ece
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-05-08 00:01:55 -0600

    añadiendo Service de equipment y añadiendo una instancia del dicho objeto en EqPlController para poder utilizar los metodos de dicho service

--- playerEquipment/src/main/java/com/playerEquip/espec/controller/EqPlController.java
+++ playerEquipment/src/main/java/com/playerEquip/espec/controller/EqPlController.java
@@ -1,6 +1,5 @@
 package com.playerEquip.espec.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
@@ -10,12 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.playerEquip.espec.models.Equipment;
 import com.playerEquip.espec.models.Player;
+import com.playerEquip.espec.services.EquipmentService;
 
 @Controller
 public class EqPlController {
 	@GetMapping("/parameters/{nombre}/{numero}")
 	public String PathVariabl(@PathVariable String nombre, @PathVariable("numero") int numero, Model model) {
-		Optional<Equipment> optionalEquipment = getEquipment().stream().filter(equipment -> nombre.toLowerCase().equals(equipment.getNombre().toLowerCase())).findFirst();
+		
+		//Instancia del objeto equipmentService
+		EquipmentService equipmentService = new EquipmentService();
+		
+		Optional<Equipment> optionalEquipment = equipmentService.getEquipment().stream().filter(equipment -> nombre.toLowerCase().equals(equipment.getNombre().toLowerCase())).findFirst();
 			if(optionalEquipment.isPresent()) {
 				Optional<Player> playerOptional = optionalEquipment.get().getPlantilla().stream().filter(player -> numero == player.getNumero()).findFirst();
 				model.addAttribute("player", playerOptional.get());
@@ -23,23 +27,4 @@ public class EqPlController {
 		return "viewPlayersAndEquipments";
 	}
 
-	public List<Equipment> getEquipment(){
-		Equipment Sidney = new Equipment();
-		Sidney.setNombre("Sidney");
-		Sidney.addPlayer(new Player("Alexandar Popovic", 41));
-		Sidney.addPlayer(new Player("Andrew Redmayne", 1));
-		Sidney.addPlayer(new Player("Harrison Devenish-Meares", 12));
-		Sidney.addPlayer(new Player("Léo Sena", 15));
-		Sidney.addPlayer(new Player("Corey Hollman", 6));
-		
-		Equipment Mexico = new Equipment();
-		Mexico.setNombre("Mexico");
-		Mexico.addPlayer(new Player("ArturoGonzález", 23));
-		Mexico.addPlayer(new Player("Roberto Alvarado", 21));
-		Mexico.addPlayer(new Player("Érick Sánchez ", 20));
-		Mexico.addPlayer(new Player("Luis Romo", 19));
-		Mexico.addPlayer(new Player("Carlos Acevedo", 27));
-		
-		return List.of(Sidney, Mexico);
-	}
 }
---
+++ playerEquipment/src/main/java/com/playerEquip/espec/services/EquipmentService.java
@@ -0,0 +1,34 @@
+package com.playerEquip.espec.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.playerEquip.espec.models.Equipment;
+import com.playerEquip.espec.models.Player;
+
+@Service
+public class EquipmentService {
+
+	//Metodo para obtener los players de un equipment
+	public List<Equipment> getEquipment(){
+		Equipment Sidney = new Equipment();
+		Sidney.setNombre("Sidney");
+		Sidney.addPlayer(new Player("Alexandar Popovic", 41));
+		Sidney.addPlayer(new Player("Andrew Redmayne", 1));
+		Sidney.addPlayer(new Player("Harrison Devenish-Meares", 12));
+		Sidney.addPlayer(new Player("Léo Sena", 15));
+		Sidney.addPlayer(new Player("Corey Hollman", 6));
+		
+		Equipment Mexico = new Equipment();
+		Mexico.setNombre("Mexico");
+		Mexico.addPlayer(new Player("ArturoGonzález", 23));
+		Mexico.addPlayer(new Player("Roberto Alvarado", 21));
+		Mexico.addPlayer(new Player("Érick Sánchez ", 20));
+		Mexico.addPlayer(new Player("Luis Romo", 19));
+		Mexico.addPlayer(new Player("Carlos Acevedo", 27));
+		
+		return List.of(Sidney, Mexico);
+	}
+	
+}
